87bddbaf383f10b4e1925aa1b23cb40509f21663
Iwannnwn <162661510+Iwannnwn@users.noreply.github.com>
2024-03-13 21:53:51 +0700

    Update Tugas1No3.java

--- Tugas1No3.java
+++ Tugas1No3.java
@@ -13,14 +13,14 @@ public class Tugas1No3 {
         System.out.println("====== i Input User ======");
         System.out.println("m(i) = " + 4*Deret(i));
 
-        // Memanggil Method jumlahan deret untuk i = {1,201,301,401}
+        // Memanggil Method jumlahan deret untuk i = {1,101,201,301,401}
         System.out.println("====== i Dari Array ======");
-        int [] x = {1,201,301,401};
+        int [] x = {1,101,201,301,401};
         for (int n = 0; n < x.length ;n++)
            System.out.println("i = " + x[n] +" | m(i) = " + 4*Deret(x[n]));
     }
     
-    // Method untuk menghitung deret ke-n
+    // Method untuk menghitung jumlahan deret ke-i
     public static double Deret(int i){
         double jumlah = 0;
         for (int n = 1; n <= i; n++){
